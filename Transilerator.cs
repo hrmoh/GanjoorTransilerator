dfadca76cb8f5dbb092e7ff9bad0303a7eefc5e6
Hamid Reza Mohammadi <mohammadi.hr@gmail.com>
2024-08-15 11:14:45 +0330

    CleanTextForTransileration

--- Transilerator.cs
+++ Transilerator.cs
@@ -4,7 +4,28 @@
     {
         public static string Trasilerate(string input)
         {
+            input = CleanTextForTransileration(input);
+
+            //TODO: add transileration code here
+
+
             return input;
+        }
+
+        public static string CleanTextForTransileration(string text)
+        {
+            if (string.IsNullOrEmpty(text)) return text;
+            return text.Replace("‌", " ")//replace zwnj with space
+                       .Replace("ّ", "")//tashdid
+                       .Replace("َ", "")//a
+                       .Replace("ِ", "")//e
+                       .Replace("ُ", "")//o
+                       .Replace("ً", "")//an
+                       .Replace("ٍ", "")//en
+                       .Replace("ٌ", "")//on
+                       .Replace("ٔ", "")//ye
+                       .Replace("ْ", "")//sokoon
+                       ;
         }
     }
 }
